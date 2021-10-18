2087d84e8c6e67996495c047a318e58c2bfd148e
Mélyssa Auberton <melyssa.auberton@gmail.com>
2021-10-18 20:33:28 +0200

    ajout des champs pour le customer dans le checkout

---
+++ src/components/CheckoutForm/FormInput.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { TextField, Grid } from '@material-ui/core';
+import { useFormContext, Controller } from 'react-hook-form';
+
+const FormInput = ({ name, label, required }) => {
+    const { control }= useFormContext();
+    return (
+        <Grid item xs={12} sm={6}>
+            <Controller 
+                // as={TextField}
+                // control={control}
+                // fullWidth
+                // name={name}
+                // label={label}
+                // require={required}
+                // render={({ field }) => <TextField {...field} />}
+                name={name}
+                control={control}
+                render = {({ field })=> (
+                    <TextField
+                        fullWidth
+                        label={label}
+                        required
+                    />
+                )}
+            />
+        </Grid>
+    )
+};
+
+export default FormInput;
