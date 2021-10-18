6862879bf28d2fa056db5f8a696136d9a39e17e5
Mélyssa Auberton <melyssa.auberton@gmail.com>
2021-10-18 16:06:56 +0200

    ajout des fonctionalités sur les boutons

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -3,7 +3,9 @@ import { Typography, Button, Card, CardActions, CardContent, CardMedia } from '@
 
 import useStyles from './styles';
 
-const CartItem = ({ item }) => {
+// si on avait encore plus de props, ce serait bien de la convertir en contexte avec react
+
+const CartItem = ({ item, onUpdateCartQty, onRemoveFromCart }) => {
     const classes= useStyles();
 
     console.log(item)
@@ -16,11 +18,11 @@ const CartItem = ({ item }) => {
             </CardContent>
             <CardActions className={classes.cardActions}>
                 <div className={classes.buttons}>
-                    <Button type="button" size="small">-</Button>
+                    <Button type="button" size="small" onClick={()=> onUpdateCartQty(item.id, item.quantity - 1)}>-</Button>
                     <Typography>{item.quantity}</Typography>
-                    <Button type="button" size="small">+</Button>
+                    <Button type="button" size="small" onClick={()=> onUpdateCartQty(item.id, item.quantity + 1)}>+</Button>
                 </div>
-                <Button type="button" variant="contained" color="secondary">Remove</Button>
+                <Button type="button" variant="contained" color="secondary" onClick={()=> onRemoveFromCart(item.id)}>Remove</Button>
             </CardActions>
         </Card>
     )
