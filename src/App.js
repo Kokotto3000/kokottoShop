5af5000849d41c69d957d76148e55dfb2a3be661
Mélyssa Auberton <melyssa.auberton@gmail.com>
2021-10-19 13:52:44 +0200

    ajout des countries dans le select du address form

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ const App= ()=> {
                         />
                     </Route>
                     <Route exact path="/checkout">
-                        <Checkout />
+                        <Checkout cart={cart}/>
                     </Route>
                 </Switch>   
             </div>
--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,20 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
 import { useForm, FormProvider } from 'react-hook-form';
 
-import Commerce from '@chec/commerce.js';
+import { commerce } from '../../lib/commerce';
 
 import FormInput from './FormInput';
 
-const AddressForm = () => {
-    // const [shippingCountries, setShippingCountries]= useState([]);
-    // const [shippingCountry, setShippingCountry]= useState('');
+const AddressForm = ({ checkoutToken }) => {
+    const [shippingCountries, setShippingCountries]= useState([]);
+    const [shippingCountry, setShippingCountry]= useState('');
     // const [shippingSubdivisions, setShippingSubdivisions]= useState([]);
     // const [shippingSubdivision, setShippingSubdivision]= useState('');
     // const [shippingOptions, setShippingOptions]= useState([]);
     // const [shippingOption, setShippingOption]= useState('');
     
     const methods = useForm();
+
+    const fetchShippingCountries= async (checkoutTokenId)=> {
+        const { countries }= await commerce.services.localeListShippingCountries(checkoutTokenId);
+
+        console.log(countries);
+        setShippingCountries(countries);
+        setShippingCountry(Object.keys(countries)[0]);
+        //[FR,DE,US]
+    }
+
+    useEffect(() => {
+        fetchShippingCountries(checkoutToken.id);
+    }, []);
 
     return (
         <>
@@ -29,16 +42,20 @@ const AddressForm = () => {
                         <FormInput required name='city' label='City' />
                         <FormInput required name='zip' label='ZIP / Postal Code' />
                     
-                        {/* <Grid item xs={12} sm={6}>
+                        <Grid item xs={12} sm={6}>
                             <InputLabel>Shipping Country</InputLabel>
-                            <Select value={} fullWidth onChange={}>
-                                <MenuItem key={} value={}>
-                                    Select Me
-                                </MenuItem>
+                            <Select value={shippingCountry} fullWidth onChange={e=> setShippingCountry(e.target.value)}>
+                                {/* on ne peut pas map un object donc on utilise d'abord Object.entries qui crée un tableau avant */}
+                                {Object.entries(shippingCountries).map(([code, name])=> ({ id: code, label: name })).map(item=> (
+                                    <MenuItem key={item.id} value={item.id}>
+                                        {item.label}
+                                    </MenuItem>
+                                ))}
+                                
                             </Select>                        
                         </Grid>     
 
-                        <Grid item xs={12} sm={6}>
+                        {/* <Grid item xs={12} sm={6}>
                             <InputLabel>Shipping Subdivision</InputLabel>
                             <Select value={} fullWidth onChange={}>
                                 <MenuItem key={} value={}>
